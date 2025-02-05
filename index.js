0c101e404cbdcd25ca15a013a70f1e556d010698
Renato Batel <renato.batel@ibm.com>
2025-02-05 17:41:08 -0300

    aula dia 01

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('Hello, world!');
+function add(num1, num2) {
+    return num1 + num2;
+}
+var result = add(5, 10);
+console.log(result); // 15   // 5 + 10 = 15  
--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-console.log('Hello, world!');
+function add(num1: number, num2: number) {
+    return num1 + num2;
+}  
+
+let result = add(5, 10); 
+console.log(result)// 15   // 5 + 10 = 15  
