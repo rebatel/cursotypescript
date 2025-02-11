c9527e5d727aec3e008b103b689950e4bd09c7ef
Renato Batel <renatobatel@Renatos-MacBook-Pro.local>
2025-02-11 15:12:36 -0300

    exercicio 04, dia 03

--- code/index.ts
+++ code/index.ts
@@ -1,6 +1,27 @@
-// Day 3 - Exercise 3
+// Day 3 - Exercise 4
 
-let isDouglas: boolean = false
+const integer: number = 6;
+const float: number = 6.66;
+const hex: number = 0xf00d;
+const binary: number = 0b1010011010;
+const octal: number = 0o744;
+const negZero:number = -0;
+const actuallyNumber:number = NaN;
+const largestNumber: number = Number.MAX_VALUE;
+const mostBiglyNumber: number = Infinity;
 
-console.log(`${isDouglas ? 'Oh, Hi Douglas' : 'Who are you?'}`)
+const members: number[] = [
+  integer,
+  float,
+  hex,
+  binary,
+  octal,
+  negZero,
+  actuallyNumber,
+  largestNumber,
+  mostBiglyNumber
+];
 
+members[0] = 12345;
+
+console.log(members);
