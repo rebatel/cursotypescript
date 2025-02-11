4fd18bcf5e5b978d4e0a6203748fd8237a3addd4
Renato Batel <renatobatel@Renatos-MacBook-Pro.local>
2025-02-11 10:37:58 -0300

    aula dia 03

--- code/index.ts
+++ code/index.ts
@@ -1,6 +1,16 @@
-
-function generateError(message: string, code: number): never {
-    throw { message: message, errorCode: code }
+function userImput(input1: number| string, input2: number| string){
+    let result
+    if (typeof input1 === "number" && typeof input2 === 'number') {
+        result = input1 + input2
+    }
+    else {
+        result = input1.toString() + input2.toString()
+    }
+    return result
 }
 
-console.log('The application crashed', 500)
+const combinePrices = userImput(10, 20)
+console.log(combinePrices)
+
+const combineName= userImput('Apple', 'Avocado')
+console.log(combineName)
--- prod/index.js
+++ prod/index.js
@@ -1,5 +1,15 @@
 "use strict";
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
+function userImput(input1, input2) {
+    let result;
+    if (typeof input1 === "number" && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
-generateError('The application crashed', 500);
+const combinePrices = userImput(10, 20);
+console.log(combinePrices);
+const combineName = userImput('Apple', 'Avocado');
+console.log(combineName);
