87bb4261aedae27fa7c172f9940be74eced2bb85
Renato Batel <renatobatel@Renatos-MacBook-Pro.local>
2025-02-13 14:21:12 -0300

    aula dia 04

--- code/index.ts
+++ code/index.ts
@@ -1,8 +1,3 @@
-// Day 3 - Exercise 4
 
-const sequence: number[] = Array.from(Array(10).keys());
-const animals: string[] = ['pangolin', 'aardvark', 'echidna', 'binturong'];
-const stringsAndNumbers: (string | number)[] = [1, 'one', 2, 'two', 3, 'three'];
-const allMyArrays: (number[] | string[] | (string | number)[])[] = [sequence, animals, stringsAndNumbers];
-
-console.log(allMyArrays);
+let code: number = 30
+console.log(code)
--- prod/index.js
+++ prod/index.js
@@ -1,7 +1,3 @@
 "use strict";
-// Day 3 - Exercise 4
-const sequence = Array.from(Array(10).keys());
-const animals = ['pangolin', 'aardvark', 'echidna', 'binturong'];
-const stringsAndNumbers = [1, 'one', 2, 'two', 3, 'three'];
-const allMyArrays = [sequence, animals, stringsAndNumbers];
-console.log(allMyArrays);
+let code = 30;
+console.log(code);
