13a623dd3e53a1231ab3ea511980e15eeb87cfb3
Renato Batel <renatobatel@Renatos-MacBook-Pro.local>
2025-02-14 18:44:39 -0300

    aula 2, dia 05

--- dist/index.js
+++ dist/index.js
@@ -4,8 +4,13 @@ class Users {
         this.name = n;
         this.balance = b;
     }
+    addMoney(amount) {
+        this.balance += amount;
+    }
 }
 const user1 = new Users("Renato", 10);
+user1.addMoney(100);
 const user2 = new Users("Amanda", 20);
+user2.addMoney(100);
 console.log(user1);
 console.log(user2);
