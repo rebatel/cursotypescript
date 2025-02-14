5c19ac1dbcc214f21c915efae95e258fa48ec0d6
Renato <renato.batel@ibm.com>
2025-02-14 19:12:24 -0300

    curso typescript

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
-interface IPerson {
-    name: string;
-    age: number;
+class MC {
+    greet(event = 'party') {
+      return `Welcome to the ${event}`;
+    }
   }
-  
-  class Person implements IPerson {  // A classe Person implementa a interface IPerson
-    constructor(public name: string, public age: number) {}
-  }
-  
-  const jane: IPerson = new Person('Jane', 31); // 'jane' é tipada com IPerson
-  const john: IPerson = {name: "John", age: 42}; // Também podemos usar diretamente
-  
-  console.log(`${jane.name} is ${jane.age} years old.`);
-  console.log(`${john.name} is ${john.age} years old.`);
+
+  const mc = new MC();
+  console.log(mc.greet('show'));
