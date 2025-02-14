50b56fa527750ff9d91bdb1ce234396b0c7690c3
Renato Batel <renatobatel@Renatos-MacBook-Pro.local>
2025-02-14 18:41:14 -0300

    aula 1 dia 05

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,11 @@
 "use strict";
-let code = 70;
-console.log(code);
+class Users {
+    constructor(n, b) {
+        this.name = n;
+        this.balance = b;
+    }
+}
+const user1 = new Users("Renato", 10);
+const user2 = new Users("Amanda", 20);
+console.log(user1);
+console.log(user2);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
+class Users {
+    name: string;
+    balance: number;
 
-let code: number = 70
-console.log(code)
+    constructor(n: string, b: number){
+        this.name = n;
+        this.balance = b;
+    }
+}
+
+const user1 = new Users("Renato", 10)
+const user2 = new Users("Amanda", 20)
+console.log(user1);
+console.log(user2);
