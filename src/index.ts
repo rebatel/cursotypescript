d4beab80c901a4a2e2ca00c9b559df015a13ae6c
Renato <renato.batel@ibm.com>
2025-02-17 15:55:50 -0300

    aula 03, dia 06

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 
 class Movie {
-    id: number;
-    name: string;
+   //readonly id: number;
+    //name: string;
 
-    constructor(id: number, name: string,) {
-        this.id = id;
-        this.name = name;
+    constructor(public readonly id: number, public name: string, private _price: number) {
+        //this.id = id;
+        //this.name = name;
     }
 }
 
-let movie1 = new Movie(1, "Forrest Gump")
-movie1.id = 5
+let movie1 = new Movie(1, "Forrest Gump", 20)
 console.log(movie1);
